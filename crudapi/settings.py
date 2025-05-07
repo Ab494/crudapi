157daf3e453e233052db2a11094da48941b988b5
Evans kipngeno <cheruiyotevans646@gmail.com>
2025-05-07 15:47:43 +0300

    Fixed Procfile and settings for Render deployment

--- crudapi/settings.py
+++ crudapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-brq01uz$_19rjg(mxh%+071lmy8u_p-w4b7#asua%sp@!=chl7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
